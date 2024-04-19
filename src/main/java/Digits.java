74eea74a58645ac74f93928868e379d7287edf29
Dannylui58 <143028255+Dannylui58@users.noreply.github.com>
2024-04-18 21:53:57 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -3,31 +3,32 @@ import java.util.ArrayList;
 public class Digits
 {
 
-	private ArrayList<Integer> digitList;
+    private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    List = new ArrayList <Integer>();
-		if(num==0)
-			List.add(0);
-		int i = num;
-		while(i>0){
-			List.add(0,i%10);
-			n=n/10;
-		}
-	}
+    public Digits(int num)
+    { /* to be implemented in part (a) */ 
+        digitList = new ArrayList<Integer>();
+        if (num==0)
+        digitList.add(0);
+        int n = num;
+        while (n>0){
+        digitList.add(0,n%10);
+        n = n/10;
+        }
+    }
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		for(int i =0;i<List.size()-1;i++)
-			if(List.get(i+1)<=List.get(i))
-				return false;
-		return true;
+    public boolean isStrictlyIncreasing()
+    { /* to be implemented in part (b) */
+    for (int i = 0; i < digitList.size()-1;i++){
+    if (digitList.get(i+1)<=digitList.get(i))
+        return false;
+    }
+        return true;
 
-	}
-	
-	public String toString()
-	{
-		return digitList.toString();
-	}
+    }
+    
+    public String toString()
+    {
+        return digitList.toString();
+    }
 }
